c9a8fe6936ecc7a54d1b6e7a0be91fa332fbbbf0
Kmicekk <91647494+Kmicekk@users.noreply.github.com>
2024-05-16 16:20:52 -0700

    new

--- tablesummary.R
+++ tablesummary.R
@@ -1,1 +1,12 @@
 # table summary
+library(tidyverse)
+library(dplyr)
+
+table1 <- read.csv(file.choose(), header = TRUE)
+
+table1$Frequency.of.Social.Media.Interaction
+table1$Self.reported.Mental.Health.Status
+
+table(table1$Frequency.of.Social.Media.Interaction, table1$Self.reported.Mental.Health.Status)
+
+
