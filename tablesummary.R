e9202a230b30d4e628f5c03715c9ccc04394563d
VitchayahS <sriv0124@uw.edu>
2024-05-17 15:56:11 -0700

    Completing table summary

--- tablesummary.R
+++ tablesummary.R
@@ -1,13 +1,14 @@
 # table summary
 library(tidyverse)
-library(dplyr)
+library(glue)
 
-table1 <- read.csv(file.choose(), header = TRUE)
-
-table1$Frequency.of.Social.Media.Interaction
-table1$Self.reported.Mental.Health.Status
-
-table(table1$Frequency.of.Social.Media.Interaction, table1$Self.reported.Mental.Health.Status)
+mental_health_data <- read.csv("Digital Behavior and Mental Health Survey 2022.csv")
+summary_table <- mental_health_data %>%
+  group_by(Frequency.of.Social.Media.Interaction) %>%
+  summarize(
+    avg_age = round(mean(Age, na.rm = TRUE), 1),
+    avg_mental_health_score = round(mean(Impact.on.Mental.Health..Score., na.rm = TRUE), 1)
+  )
 
 
 
